965da823321dc9081e192fe71ea9f54d32a646f8
Peter Thomas <thomasp@macstudent.com.au>
2026-08-19 08:53:04 +1000

    Setup file framework

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-import os
+import os, csv, io
 
 app = Flask(__name__)
 
@@ -13,18 +13,18 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 def index():
     return render_template("index.html")
 
-
 @app.route("/upload", methods=["POST"])
 def upload_file():
     file = request.files.get("file")
 
-    if not file or file.filename == "":
-        return "No file selected"
+    if not file:
+        return "No file uploaded"
 
-    file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
-
-    return redirect(url_for("index"))
-
+    text = file.read().decode("utf-8")
+    reader = csv.DictReader(io.StringIO(text))
+    for row in reader:
+        print(row)
+    return "CSV read successfully"
 
 if __name__ == "__main__":
     app.run(debug=True)
